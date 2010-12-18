2b39ac7568453a5447b3147fc5f9957bb20d146d
Ubuntu <ubuntu@ip-10-120-62-127.ec2.internal>
2010-12-18 11:21:42 -0800

    added database posting

--- admin.php
+++ admin.php
@@ -14,6 +14,9 @@ if ( !( $db = mysql_connect($dbserver, $dbuser, $dbpword) ) ) {
 
 if ( isset($_POST['newpost']) && $_POST['newpost'] == "1" ) {
   echo "posted new message...\n";
+  mysql_db_query("blog", "CREATE TABLE IF NOT EXISTS posts (id int(8) PRIMARY KEY AUTO_INCREMENT, title VARCHAR(256), author VARCHAR(256), body VARCHAR(20000), tstamp timestamp DEFAULT CURRENT_TIMESTAMP);", $db);
+
+  if (!mysql_db_query("blog", "INSERT INTO posts (title, author, body) VALUES('$_POST[title]','$_POST[author]','$_POST[body]');")) die("Could not insert into db...");
 }
 
 ?>
